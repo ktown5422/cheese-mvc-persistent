b2808b87226dec28b9668909183256254e5f0ebb
ktown5422 <ktown5422@gmail.com>
2019-09-17 11:27:07 -0500

    cheese mvc part 2 - delete cheese class

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -3,6 +3,7 @@ package org.launchcode.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -21,8 +22,11 @@ public class Cheese {
     private String name;
 
     @NotNull
-    @Size(min=1, message = "Description must not be empty")
+    @Size(min=1, message = "The Description cant be empty")
     private String description;
+
+    @ManyToOne
+    private Category category;
 
     private CheeseType type;
 
