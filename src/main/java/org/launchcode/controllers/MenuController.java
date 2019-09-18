1a235f56da3289d12a47b594771db00a753f18e3
ktown5422 <ktown5422@gmail.com>
2019-09-18 17:53:34 -0500

    cheese mvc persistent part 3 - add displayaddmenuform and proccessaddmenuform to menu controller

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,14 +1,20 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping
+@RequestMapping(value = "menu")
 public class MenuController {
     @Autowired
     private CheeseDao cheeseDao;
@@ -22,4 +28,27 @@ public class MenuController {
 
         return "menu/index";
     }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddMenuForm(Model model){
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+
+        return "menu/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddMenuForm(@ModelAttribute @Valid Menu menu, Errors errors, Model model){
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+
+        } else {
+            menuDao.save(menu);
+
+            return "redirect:view/" + menu.getId();
+        }
+    }
+
+
 }
